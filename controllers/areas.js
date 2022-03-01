b381c64052d5525ea84a3a95cf580866f36662d9
mvanzo <martin.vanzo@gmail.com>
2022-03-01 09:01:14 -0800

    weather API connect and pulling data to EJS

--- controllers/areas.js
+++ controllers/areas.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { route } = require('express/lib/application')
 const router = express.Router()
 const db = require('../models')     // link to database
+const axios = require('axios')
 require('dotenv').config()
 
 router.get('/', (req, res)=> {
@@ -14,11 +15,63 @@ router.get('/:id', async (req, res)=> {
             where: {id: req.params.id}
         })
 
-        res.render('./areas/index.ejs', {backcountryLocation})
+        res.render('./areas/index.ejs', {
+            location: backcountryLocation.name,
+            
+        })
+
     } catch (err) {
         console.log('error finding the right ski area', err)
     }
+})
+
+router.get('/:id/weather', async (req, res)=>{
+    try {
+        const backcountryLocation = await db.area.findOne({
+            where: {id: req.params.id}
+        })
+
+        let findWeather = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${backcountryLocation.latitude}%2C${backcountryLocation.longitude}?unitGroup=us&key=R7SPHSWWT6TMBQ7SYQYPFD985&contentType=json`)
+                
+        res.json(findWeather.data.days[0])
+    
+        res.render('./areas/weather.ejs', {
+            // CURRENT CONDITIONS
+            currentConditions: findWeather.data.currentConditions.conditions,
+            currentTemp: findWeather.data.currentConditions.temp,
+            currentWindSpeed: findWeather.data.currentConditions.windspeed,
+            currentWindDir: findWeather.data.currentConditions.winddir,
+            currentCloudCover: findWeather.data.currentConditions.cloudcover,
+            currentSolarRadiation: findWeather.data.currentConditions.solarradiation,
+            currentCloudCover: findWeather.data.currentConditions.solarenergy,
+            currentUvIndex: findWeather.data.currentConditions.uvindex,
+
+            // TODAY
+            todayDescription: findWeather.data.days[0].description,
+            todayPrecipProb: findWeather.data.days[0].precipprob,
+            todayTempMax: findWeather.data.days[0].tempmax,
+            todayTempMin: findWeather.data.days[0].tempmin,
+            currentPrecipType: findWeather.data.days[0].preciptype,   // this is an array
+            
+            // SNOWPACK
+            snowDepth: findWeather.data.days[0].snowdepth,
+
+            // FORECAST
+
+            // HISTORY
+
+            // ALERTS
+
+        })
+
+    } catch (err) {
+        console.log('error finding the weather', err)
+    }
 
 })
+
+
+
+
 
 module.exports = router
