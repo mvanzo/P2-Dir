96bd2f460306a7b107dd421a2be43edda500377c
mvanzo <martin.vanzo@gmail.com>
2022-03-06 18:28:30 -0800

    current and seven day forecast for favorited area

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -21,6 +21,116 @@ router.get('/new', async (req, res)=> {
     }
 })
 
+// READ selected favorite current weather
+router.get('/:name/current', async (req, res)=> {
+    try {
+        const favoriteArea = await db.favorite.findOne({
+            where: {name: req.params.name}
+        })
+        let findWeather = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${favoriteArea.latitude}%2C${favoriteArea.longitude}?unitGroup=us&key=${process.env.WEATHER_API_KEY}&contentType=json`)             
+
+        // description
+        let dayDescription = await (findWeather.data.description)
+
+        // OBJECT TO PULL DATA FROM
+        let hourlyWeather = await findWeather.data.days[0].hours
+        
+        // hours array for x-axis of all charts
+        let hours = await (hourlyWeather).map(el=> {return el.datetime})
+        
+        //temp chart
+        let temps = await (hourlyWeather).map(el=> {return el.temp})
+        
+        // wind chart
+        let windSpeed = await (hourlyWeather).map(el=> {return el.windspeed})
+        let windGusts = await (hourlyWeather).map(el=> {return el.windgust})
+        
+        // precip chart
+        let precip = await (hourlyWeather).map(el=> {return el.precip})
+
+        // percentages chart
+        let cloudCover = await (hourlyWeather).map(el=> {return el.cloudcover})
+        let precipProb = await (hourlyWeather).map(el=> {return el.precipprob})
+
+        res.render('./areas/currentweather.ejs', {
+            location: favoriteArea.name,
+
+            // DESCRIPTION
+            dayDescription,
+
+            // CURRENT CONDITIONS
+            currentConditions: findWeather.data.currentConditions.conditions,
+            currentTemp: findWeather.data.currentConditions.temp,
+            currentFeelsLike: findWeather.data.currentConditions.feelslike,
+            currentWindSpeed: findWeather.data.currentConditions.windspeed,
+            currentWindDir: findWeather.data.currentConditions.winddir,
+            currentCloudCover: findWeather.data.currentConditions.cloudcover,
+
+            // HOURLY
+            hours,
+            temps,
+            windSpeed,
+            windGusts,
+            precip,
+            cloudCover,
+            precipProb,
+            
+            // SNOWPACK
+            snowDepth: findWeather.data.days[0].snowdepth,
+        })
+    } catch (err) {
+        console.log('there was an error finding the current forecast for favorite area', err)
+    }
+})
+
+// READ selected favorite weather forecast
+router.get('/:name/forecast', async (req, res)=> {
+    try {
+        const favoriteArea = await db.favorite.findOne({
+            where: {name: req.params.name}
+        })
+        // ajax weather forecast
+        let forecastUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${favoriteArea.latitude}%2C${favoriteArea.longitude}/next7days?unitGroup=us&key=${process.env.WEATHER_API_KEY}&contentType=json`
+        let findWeather = await axios.get(forecastUrl)
+        
+        // dates array for x-axis of all charts
+        let dates = await (findWeather.data.days).map(el=> {return el.datetime})
+        
+        // temp chart
+        let highTemps = await (findWeather.data.days).map(el=> {return el.tempmax}) 
+        let lowTemps = await (findWeather.data.days).map(el=> {return el.tempmin})
+
+        // wind chart
+        let windSpeeds = await (findWeather.data.days).map(el=> {return el.windspeed})
+        let windGusts = await (findWeather.data.days).map(el=> {return el.windgust})
+        
+        // precip chart
+        let precip = await (findWeather.data.days).map(el=> {return el.precip})
+
+        // percentages chart
+        let cloudCover = await (findWeather.data.days).map(el=> {return el.cloudcover})
+        let precipProb = await (findWeather.data.days).map(el=> {return el.precipprob})
+        
+
+        res.render('./areas/forecast.ejs', {
+            location: favoriteArea.name,
+            forecastDescription: findWeather.data.description,
+            forecastDays: findWeather.data.days,
+            dates,
+            highTemps,
+            lowTemps,
+            windSpeeds,
+            windGusts,
+            precip,
+            cloudCover,
+            precipProb
+        })
+
+    } catch (err) {
+        console.log('there was an error finding the forecast', err)
+    }
+})
+
 // CREATE new fave location
 router.post('/', async (req, res)=> {
     try {
